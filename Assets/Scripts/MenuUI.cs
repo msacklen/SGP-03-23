76fbfe02eb111f2646da21125e3c71f9a1dcf940
ArctosFI <arttu.niemi@live.fi>
2023-03-26 19:27:40 +0300

    Made working main menu
    Placeholder main menu works correctly

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] TMP_Text ipText;
+    [SerializeField] NetworkManager netManager;
+    [SerializeField] UnityTransport utp;
+    NetworkSceneManager SceneManager;
+    string ip = "";
+
+    public void Update()
+    {
+        ipText.SetText(ip);
+    }
+
+    public void Host()
+    {
+        netManager.StartHost();
+        SceneManager.LoadScene("Lobby",LoadSceneMode.Single);
+    }
+
+    public void Join()
+    {
+        utp.SetConnectionData(ip, 7777);
+        netManager.StartClient();
+        SceneManager.LoadScene("Lobby", LoadSceneMode.Single);
+    }
+
+    #region Keyboard inputs
+    public void KB_dot()
+    {
+        ip += ".";
+    }
+    public void KB_back()
+    {
+        ip = ip.Remove(ip.Length-1,1);
+    }
+    public void KB_0()
+    {
+        ip += "0";
+    }
+    public void KB_1()
+    {
+        ip += "1";
+    }
+    public void KB_2()
+    {
+        ip += "2";
+    }
+    public void KB_3()
+    {
+        ip += "3";
+    }
+    public void KB_4()
+    {
+        ip += "4";
+    }
+    public void KB_5()
+    {
+        ip += "5";
+    }
+    public void KB_6()
+    {
+        ip += "6";
+    }
+    public void KB_7()
+    {
+        ip += "7";
+    }
+    public void KB_8()
+    {
+        ip += "8";
+    }
+    public void KB_9()
+    {
+        ip += "9";
+    }
+    #endregion
+}
--- Assets/Scripts/NetworkStartup.cs
+++ Assets/Scripts/NetworkStartup.cs
@@ -1,17 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class NetworkStartup : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
