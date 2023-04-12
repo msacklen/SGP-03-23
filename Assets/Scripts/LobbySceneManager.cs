dfb1feb600349c48f52535ef77d037cdfa63016a
Arttu <arttu.niemi@live.fi>
2023-04-12 13:35:59 +0300

    Host ip shown in lobby

--- Assets/Scripts/LobbySceneManager.cs
+++ Assets/Scripts/LobbySceneManager.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Net;
+using System.Linq;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class LobbySceneManager : NetworkBehaviour
 {
     [SerializeField] GameObject sceneSwitchCanvas;
+    [SerializeField] TMP_Text textIP;
 
     private void Start()
     {
         if (!IsHost) sceneSwitchCanvas.SetActive(false);
+        else
+        {
+            IPAddress[] localIPs = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+            string hostIP = localIPs.FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)?.ToString();
+            textIP.text = hostIP;
+        }
     }
 
     public void LoadApartment()
