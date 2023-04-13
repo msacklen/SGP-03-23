0509d28dba1475abef3c4d2d6c4e757c0778b8a1
BigKlepa <71874966+BigKlepa@users.noreply.github.com>
2023-04-13 10:22:01 +0300

    some pzzles

---
+++ Assets/Scripts/avain.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avain : MonoBehaviour
+{
+    [SerializeField] private GameObject lid;
+    [SerializeField] private GameObject puzzle;
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Key")
+        {
+            Destroy(lid);
+            puzzle.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/puzzleLogic.cs
+++ Assets/Scripts/puzzleLogic.cs
@@ -7,22 +7,22 @@ public class puzzleLogic : MonoBehaviour
 {
     [SerializeField] private Vector3 lockedPlace;
     [SerializeField] private Vector3 lockedRotation = new Vector3(-90, 0, 0);
+    ///public Transform puzzle;
     // Start is called before the first frame update
     private int total;
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(lockedPlace, transform.localPosition) < 0.33f)
+        if (Vector3.Distance(lockedPlace, transform.position) < 0.33f)
         {
-            transform.localPosition = lockedPlace;
+            transform.position = lockedPlace;
             transform.eulerAngles = lockedRotation;
             GetComponent<Rigidbody>().isKinematic = true;
 
             total += 1;
             this.gameObject.GetComponent<puzzleLogic>().enabled = false;
             GetComponent<XRGrabInteractable>().enabled = false;
-            Debug.Log(total);
         }
     }
 }
