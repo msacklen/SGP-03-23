63e369da9809367e088e721c34443958a5cfbbae
BigKlepa <71874966+BigKlepa@users.noreply.github.com>
2023-04-19 13:56:32 +0300

    avain

--- Assets/Scripts/pihditt.cs
+++ Assets/Scripts/pihditt.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class pihditt : MonoBehaviour
 {
+    public Transform pihdit;
     public Transform paikkaavaimelle;
+    private bool isActivated = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +16,20 @@ public class pihditt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, paikkaavaimelle.position) < 0.2f)
+        if (Vector3.Distance(transform.position, paikkaavaimelle.position) < 0.2f && isActivated == true)
         {
+            transform.parent = pihdit;
             transform.position = paikkaavaimelle.position;
         }
+    }
+
+    public void activated()
+    {
+        isActivated = true;
+    }
+
+    public void deactivate()
+    {
+        isActivated = false;
     }
 }
