dbcdb2cb67e55357db32f42f95c9dae9b13f9e59
BigKlepa <tuomas.suni@edu.turkuamk.fi>
2023-05-08 14:12:28 +0300

    puzzles

--- Assets/Scripts/puzzleLogic.cs
+++ Assets/Scripts/puzzleLogic.cs
@@ -15,7 +15,7 @@ public class puzzleLogic : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(lockedPlace, transform.position) < 0.33f && IsOwnedByServer && transform.parent != null)
+        if (Vector3.Distance(lockedPlace, transform.position) < 0.33f && IsOwnedByServer)
         {
             Debug.Log("Puzzlepiece in place");
             transform.position = lockedPlace;
